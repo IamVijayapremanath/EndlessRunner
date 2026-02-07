d9edd643176322a338eb209d4ef13e7ce2311965
unknown <vijayapremanath@gmail.com>
2026-02-07 22:53:25 +0530

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float forward_speed = 5f;
+    public float side_speed = 5f;
+    public float jump_force = 5f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * forward_speed * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * side_speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * side_speed * Time.deltaTime);
+        }
+    }
+}
